916b9c3cefe4b13377d392a3d606fa0de1f6af80
Jrope21 <joshbball8@gmail.com>
2019-12-18 21:30:45 -0600

    setting up plugins -- adding global styles

---
+++ src/components/navigation/navigation.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './navigation.styles.scss'
+
+function Navigation() {
+
+    return (
+        <header>
+            <nav>
+
+            </nav>
+        </header>
+    )
+}
+
+export default Navigation;
