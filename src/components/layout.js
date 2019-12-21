c6ae414526c2088cfb4f171840a9523716df25c3
Jrope21 <joshbball8@gmail.com>
2019-12-20 23:41:37 -0600

    setting up menu sonnn

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,7 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
+import Navigation from "./navigation/navigation.component.jsx"
 import "../css/global.styles.scss";
 
 const Layout = ({ children }) => {
@@ -25,7 +25,7 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
+      <Navigation />
       <div>
         <main>{children}</main>
         <footer>
--- src/components/navigation/navigation.component.jsx
+++ src/components/navigation/navigation.component.jsx
@@ -4,7 +4,7 @@ import { Link, useStaticQuery, graphql} from 'gatsby'
 import './navigation.styles.scss'
 
 function Navigation() {
-    const { menu: { edges: [{node}]}} = useStaticQuery (
+    const { menu: { edges: [{node: menu}]}} = useStaticQuery (
         graphql`
             query menu {
                 menu: allWordpressMenusMenusItems
@@ -26,7 +26,12 @@ function Navigation() {
     return (
         <header>
             <nav>
-
+                {console.log(menu)}
+                <ul className="navigation">
+                    {menu.items.map(({title, url}) => (
+                        <li><a href={url}>{title}</a></li>
+                    ))}
+                </ul>
             </nav>
         </header>
     )
