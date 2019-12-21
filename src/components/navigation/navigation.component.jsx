94d4ac3f6c53872b5c60795a1fe56975df59082d
Jrope21 <joshbball8@gmail.com>
2019-12-20 23:35:18 -0600

    setting up wp menu query

--- src/components/navigation/navigation.component.jsx
+++ src/components/navigation/navigation.component.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { Link, useStaticQuery, graphql} from 'gatsby'
+
 import './navigation.styles.scss'
 
 function Navigation() {
+    const { menu: { edges: [{node}]}} = useStaticQuery (
+        graphql`
+            query menu {
+                menu: allWordpressMenusMenusItems
+                    {
+                    edges {
+                        node {
+                            items{
+                                title
+                                url
+                            }
+                        }
+                    }
+                }
+            }
+        `
+    )
+
 
     return (
         <header>
