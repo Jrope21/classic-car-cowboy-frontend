f4577eedf6d6f3a6723d1a127232207096a9c6a7
jrope21 <joshbball8@gmail.com>
2019-12-25 00:33:38 -0600

    CHRISTMASSSS -- finishing hamburger, fixing other nav issues

--- src/components/navigation/hamburger/hamburger.component.jsx
+++ src/components/navigation/hamburger/hamburger.component.jsx
@@ -4,8 +4,8 @@ import './hamburger.styles.scss';
 function Hamburger({active, ...otherProps}) {
 
     return (
-        <div {...otherProps} class="hamburger-container">
-            <span aria-label="A hamburger icon to toggle the navigation" className={`${active} hamburger`}></span>
+        <div {...otherProps} class={`hamburger-container ${active}`} >
+            <span aria-label="A hamburger icon to toggle the navigation" className={`hamburger`}></span>
         </div>
     )
 }
