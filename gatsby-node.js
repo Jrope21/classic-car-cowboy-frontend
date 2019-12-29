73cfece157e3222648632c4afa8418590a78bb49
Jrope21 <joshbball8@gmail.com>
2019-12-28 18:52:32 -0600

    testing gatsby-node shiz

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,34 +1,67 @@
+// const path = require(`path`)
+// const slash = require(`slash`)
+// exports.createPages = async ({ graphql, actions }) => {
+//   const { createPage } = actions
+//   // query content for WordPress posts
+//   const result = await graphql(`
+//     query {
+//       allWordpressPage {
+//         edges {
+//           node {
+//             id
+//             slug
+//             acf
+//           }
+//         }
+//       }
+//     }
+//   `)
+//   const data = await result;
+//   console.log('data doe', data);
+//   const pageTemplate = path.resolve(`./src/templates/standard.route.jsx`);
+//   await result.data.allWordpressPage.edges.forEach(edge => {
+//     createPage({
+//       // will be the url for the page
+//       path: edge.node.slug,
+//       // specify the component template of your choice
+//       component: slash(pageTemplate),
+//       // In the ^template's GraphQL query, 'id' will be available
+//       // as a GraphQL variable to query for this posts's data.
+//       context: {
+//         id: edge.node.id,
+//         acf: edge.node.acf
+//       },
+//     })
+//   })
+// }
+
 const path = require(`path`)
-const slash = require(`slash`)
-exports.createPages = async ({ graphql, actions }) => {
+exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
-  // query content for WordPress posts
-  const result = await graphql(`
-    query {
-      allWordpressPage {
+  return graphql(`
+    {
+      allWordpressPost(sort: { fields: [date] }) {
         edges {
           node {
-            id
+            title
+            excerpt
+            content
             slug
-            acf
           }
         }
       }
     }
-  `)
-  const postTemplate = path.resolve(`./src/templates/post.js`)
-  result.data.allWordpressPost.edges.forEach(edge => {
-    createPage({
-      // will be the url for the page
-      path: edge.node.slug,
-      // specify the component template of your choice
-      component: slash(postTemplate),
-      // In the ^template's GraphQL query, 'id' will be available
-      // as a GraphQL variable to query for this posts's data.
-      context: {
-        id: edge.node.id,
-        acf: edge.node.acf
-      },
+  `).then(result => {
+    result.data.allWordpressPost.edges.forEach(({ node }) => {
+      createPage({
+        path: node.slug,
+        component: path.resolve(`./src/templates/standard.route.jsx`),
+        context: {
+          // This is the $slug variable
+          // passed to blog-post.js
+          slug: node.slug,
+        },
+      })
     })
   })
 }
--- src/templates/standard.route.jsx
+++ src/templates/standard.route.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Standard({ ...props }) {
 
-    console.log(props)
+    console.log('props', props)
 
     return (
         <div className="standard">
