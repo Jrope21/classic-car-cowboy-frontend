ef35cd46d5358a65792e09d64c51cc12b6624394
jrope21 <joshbball8@gmail.com>
2020-02-16 17:36:40 -0600

    adding car listing component w/ data

--- src/components/modules/featured-cars-listing/featured-cars-listing.component.jsx
+++ src/components/modules/featured-cars-listing/featured-cars-listing.component.jsx
@@ -1,18 +1,36 @@
-// import React from 'react';
-// import './featured-cars-listing.styles.scss';
+import React from 'react';
+import './featured-cars-listing.styles.scss';
 
-// import { getSelectedCarInfoFromPostObj } from '../../../lib/_helpers.js';
+import { Link } from 'gatsby'
 
-// function FeaturedCarsListing({acf, cars}) {
-//     // const { car_selection } = acf;
-//     // const allSelectedCarInformation = getSelectedCarInfoFromPostObj({selectedCars: car_selection, allCars: cars});
-//     console.log(acf);
+import { getSelectedCarInfoFromPostObj } from '../../../lib/_helpers.js';
 
-//     return (
-//         <div className="module featured-cars-listing">
+function FeaturedCarsListing({acf, cars}) {
+    
+    const { car_selection } = acf;
+    const allSelectedCarInformation = getSelectedCarInfoFromPostObj({selectedCars: car_selection, allCars: cars});
 
-//         </div>
-//     )
-// }
+    return (
+        <div className="module featured-cars-listing container">
+            {allSelectedCarInformation.map(({slug, acf: {car_model, car_year, car_availability, car_images}}, i) => (
+                <div className="car-listing">
+                    <div className="car-thumb">
+                    <span className={`
+                        availabilty
+                        ${car_availability ? 'success' : 'error'}`}
+                    >
+                        {car_availability ? 'Available' : 'Not Available'}
+                    </span>
+                        <img src={car_images[0].image_selection.url} alt={car_images[0].image_selection.alt} />
+                    </div>
+                    <div className="car-text">
+                        <p className="model">{`${car_year}`}</p>
+                        <h4 className="car-name">{car_model}</h4>
+                    </div>
+                </div>
+            ))}
+        </div>
+    )
+}
 
-// export default FeaturedCarsListing;
+export default FeaturedCarsListing;
--- src/components/modules/module-controller.component.jsx
+++ src/components/modules/module-controller.component.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 
 import FeaturedCarsCarousel from './featured-cars-carousel/featured-cars-carousel.component.jsx';
 import ContentArea from './content-area/content-area.component.jsx';
-// import FeaturedCarsListing from './featured-cars-listing/featured-cars-listing.component.jsx';
+import FeaturedCarsListing from './featured-cars-listing/featured-cars-listing.component.jsx';
 
 function ModuleController({ acf, cars }) {
-    console.log('module controller acf', acf);
+    // console.log('module controller acf', acf);
 
     function renderCorrectLayout(acfModule, i) {
         const { acf_fc_layout : layout } = acfModule
 
         switch (layout) {
-            // case "featured_cars_listing": 
-            //     return <FeaturedCarsListing cars={cars} acf={acfModule} key={i} />
+            case "featured_cars_listing": 
+                return <FeaturedCarsListing cars={cars} acf={acfModule} key={i} />
             case "featured_cars_carousel":
                 return <FeaturedCarsCarousel cars={cars} acf={acfModule} key={i} />
             case "content_area":
