19638f2849d0ae58419cd9d6ac5fcc3df948f95e
Jrope21 <joshbball8@gmail.com>
2020-03-02 21:49:44 -0600

    working on pagination component

--- src/components/modules/all-cars-gallery/all-cars-gallery.component.jsx
+++ src/components/modules/all-cars-gallery/all-cars-gallery.component.jsx
@@ -1,17 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './all-cars-gallery.styles.scss'; 
 
 function AllCarsGallery({ cars }) {
 
-    console.log(cars)
-    const [activeCars, setActiveCars] = useState(cars.slice(0, 6));
+    let carsPerGroup = 6;
+    
+    const [activeCarGroupIndex, setActiveCarGroupIndex] = useState(0);
+    const [groupedCars, setGroupedCars] = useState(null);
+    const [activeCars, setActiveCars] = useState(null);
 
-    console.log('active cars', activeCars)
+    useEffect(() => {
+        let allCars = cars;
+        setActiveCars(allCars.slice(0, carsPerGroup))
+        setGroupedCars(splitCarsIntoGroups(allCars));
+    }, [])
 
-// TODO -- set up pagination for every 6th car
-// TODO -- style for desktops
+    function splitCarsIntoGroups(allCars, groupedCars = []) {
+        if(allCars.length < carsPerGroup) {
+            groupedCars.push([...allCars]);
+            return groupedCars;
+        }
 
-    return (
+        groupedCars.push([...allCars.slice(0, carsPerGroup)]);
+        
+        return splitCarsIntoGroups(allCars.slice(carsPerGroup), groupedCars);
+    }
+
+    function handleCarPagination(indexOfCarGroup) {
+        setActiveCarGroupIndex(indexOfCarGroup);
+        setActiveCars(groupedCars[indexOfCarGroup]);
+    }
+
+    if (activeCars && groupedCars) return (
         <div className="all-cars-gallery container module">
             <div className="all-cars-container">
                 {activeCars.map(({acf: { car_images }}) => (
@@ -21,10 +41,29 @@ function AllCarsGallery({ cars }) {
                 ))}
             </div>
             <div className="pagination">
-
+                {activeCarGroupIndex !== 0 ? 
+                    <button 
+                        className="prev"
+                        onClick={() => handleCarPagination(activeCarGroupIndex - 1)}
+                    >prev</button>
+                : null}
+                {groupedCars.map((carGroup, i) => (
+                    <span 
+                        className="pagination-number"
+                        onClick={() => handleCarPagination(i)}
+                    >{i}</span>
+                ))}
+                {groupedCars.length > 1 && activeCarGroupIndex !== groupedCars.length - 1 ? 
+                    <button 
+                        className="next"
+                        onClick={() => handleCarPagination(activeCarGroupIndex + 1)}
+                    >next</button> 
+                : null}
             </div>
         </div>
     )
+
+    return null;
 }
 
 export default AllCarsGallery;
